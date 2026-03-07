798dae77daf659ede3838f6cc1c8e85f657ef57e
AstralStoat17 <232106207+AstralStoat17@users.noreply.github.com>
2026-03-07 17:17:06 +0100

    wip: remove not gitlab code

--- lib/screens/go_search.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
-
-class GoSearchScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('Coming Soon...'));
-  }
-}
